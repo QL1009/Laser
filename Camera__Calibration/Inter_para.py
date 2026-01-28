6e836db8e67c48f2f7e796e93988209fe35f1452
QL1009 <1074097289@qq.com>
2026-01-28 16:35:07 +0800

    相机内参标定

--- Camera__Calibration/Inter_para.py
+++ Camera__Calibration/Inter_para.py
@@ -4,8 +4,8 @@ import os
 
 
 # 棋盘格内角点行列数
-
 pattern_size = (9, 6)
+
 # 假设棋盘格每个方格边长为1（实际应用中需根据真实尺寸调整,单位：米）
 square_size = 1
 
@@ -13,8 +13,8 @@ square_size = 1
 objp = np.zeros((np.prod(pattern_size), 3), np.float32)
 objp[:, :2] = np.indices(pattern_size).T.reshape(-1, 2) * square_size
 
-objpoints = []  # 存储世界坐标点
-imgpoints = []  # 存储图像坐标点
+objpoints = []  # 用于存储世界坐标点
+imgpoints = []  # 用于存储图像坐标点
 
 # 标定板照片所在文件夹
 images_folder = 'H:\PyPrj\Laser\Camera__Calibration\data'
